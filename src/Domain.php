727288d40e4e31ad00b8a6b49b46361dd777f773
ddd <ddd@qqq.com>
2021-09-08 15:35:05 +0800

    Signed-off-by: ddd <ddd@qqq.com>

--- src/Domain.php
+++ src/Domain.php
@@ -41,14 +41,66 @@ class Domain extends _Base
 
     }
 
+    /**
+     * 查询域名信息
+     *
+     * @param array $info
+     * @return array|string
+     * @throws \AlibabaCloud\Client\Exception\ClientException
+     * @throws \AlibabaCloud\Client\Exception\ServerException
+     */
+    public function read(array $info)
+    {
+        try {
+            $request = DomainAli::v20180129()->queryDomainByDomainName();
+            return $request
+                ->withDomainName($info['domain'])
+                ->debug($this->debug)
+                ->connectTimeout($this->timeout)
+                ->timeout($this->timeout)
+                ->request()
+                ->toArray();
+
+        } catch (ClientException $exception) {
+            return $exception->getMessage();
+        } catch (ServerException $exception) {
+            return $exception->getMessage();
+        }
+
+    }
+
 
     /**
+     * 续费
+     *
      * @param array $info
      * @return array|string
      * @throws \AlibabaCloud\Client\Exception\ClientException
      * @throws \AlibabaCloud\Client\Exception\ServerException
      */
     public function renew(array $info)
+    {
+        try {
+            $request = DomainAli::v20180129()->saveSingleTaskForCreatingOrderRenew();
+            return $request
+                ->withDomainName($info['domain'])
+                ->withSubscriptionDuration($info['year'])
+                ->withCurrentExpirationDate($info['expire'])
+                ->debug($this->debug)
+                ->connectTimeout($this->timeout)
+                ->timeout($this->timeout)
+                ->request()
+                ->toArray();
+
+        } catch (ClientException $exception) {
+            return $exception->getMessage();
+        } catch (ServerException $exception) {
+            return $exception->getMessage();
+        }
+
+    }
+
+    public function checkTask(array $info)
     {
         try {
             $request = DomainAli::v20180129()->saveSingleTaskForCreatingOrderRenew();
