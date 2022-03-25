94c51aa5e4ef24566140acc71d5f51d0bea1bab3
ddd <ddd@qqq.com>
2022-03-25 17:06:59 +0800

    Signed-off-by: ddd <ddd@qqq.com>

--- src/Rds.php
+++ src/Rds.php
@@ -17,21 +17,7 @@ class Rds extends _Base
     public function load()
     {
         try {
-            /**
-             * AlibabaCloud::rpc()
-             * ->product('Rds')
-             * // ->scheme('https') // https | http
-             * ->version('2014-08-15')
-             * ->action('DescribeDBInstances')
-             * ->method('POST')
-             * ->host('rds.aliyuncs.com')
-             * ->options([
-             * 'query' => [
-             * ],
-             * ])
-             * ->request();
-             */
-            return AlibabaCloud::rpc()
+            $result = AlibabaCloud::rpc()
                 ->product('Rds')
                 ->version('2014-08-15')
                 ->action('DescribeDBInstances')
@@ -42,7 +28,30 @@ class Rds extends _Base
                 ])
                 ->request()
                 ->toArray();
-
+            $rdsAll = [];
+            foreach ($result['Items']['DBInstance'] as $rs) {
+                $rds = [];
+                $rds['mid'] = $rs['MasterInstanceId'];//主实例ID
+                $rds['id'] = $rs['DBInstanceId'];//实例id
+                $rds['stype'] = $rs['DBInstanceStorageType'];//存储类型
+                $rds['lock'] = $rs['LockMode'];
+                $rds['create'] = $rs['CreateTime'];
+                $rds['expire'] = $rs['ExpireTime'];
+                $rds['paytype'] = $rs['PayType'];
+                $rds['version'] = $rs['EngineVersion'];
+                $rds['host'] = $rs['ConnectionString'];//连接地址
+                $rds['nettype'] = $rs['DBInstanceNetType'];//连接地址
+                $rds['zone'] = $rs['ZoneId'];//可用区ID。 cn-hangzhou-a
+                $rds['region'] = $rs['RegionId'];//地域ID。 cn-hangzhou
+                $rds['desc'] = $rs['DBInstanceDescription'];
+                $rds['itype'] = $rs['DBInstanceType'];//实例类型Primary：主实例，ReadOnly：只读实例
+                $rds['class'] = $rs['DBInstanceClass'];//规格
+                $rds['type'] = $rs['Engine'];//类型 mysql
+                $rds['category'] = $rs['Category'];//实例系列： Basic：基础版HighAvailability：高可用版Finance：三节点企业版
+                $rds['status'] = $rs['DBInstanceStatus'];
+                $rdsAll[] = $rds;
+            }
+            return $rdsAll;
         } catch (ClientException $exception) {
             return $exception->getMessage();
         } catch (ServerException $exception) {
