cdfa3ecd71745d587db170955d965f88bc132abc
老船长 <fazo@qq.com>
2023-08-14 23:45:03 +0800

    init

---
+++ src/Command.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace laocc\aliyun;
+
+/**
+ * 阿里云，云助手
+ *
+ * https://help.aliyun.com/zh/ecs/user-guide/overview-10?spm=a2c4g.11186623.0.0.5634768d4Z1JJj
+ *
+ */
+class Command extends _Base
+{
+    public function create()
+    {
+    }
+
+    public function query()
+    {
+    }
+
+    public function exec()
+    {
+    }
+
+    public function run()
+    {
+    }
+
+    public function delete()
+    {
+    }
+}
--- src/Mysql.php
+++ src/Mysql.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 namespace laocc\aliyun;
 
-class Mysql
+class Mysql extends _Base
 {
 
 }
